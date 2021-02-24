d3a5d06f60d56274877b4309aa70502116657b60
Rom4io <salyahov@gmail.com>
2021-02-24 10:29:30 +0300

    Day 1 updated

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        int num =0;
+        while (num <= 9){
+            System.out.print("JAVA ");
+            num++;
+        }
     }
 }
