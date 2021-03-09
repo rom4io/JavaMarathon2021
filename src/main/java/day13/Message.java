a85d028c47b49505d2c0c25b5d97255999e5cac3
Rom4io <salyahov@gmail.com>
2021-03-09 15:14:16 +0300

    Add Day12 solution

--- src/main/java/day13/Message.java
+++ src/main/java/day13/Message.java
@@ -13,6 +13,46 @@ public class Message {
         this.sender = sender;
         this.receiver = receiver;
         this.text = text;
+        this.date = new Date();
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
         this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "FROM: " + sender + "/n" +
+                "TO: " + receiver + "/n" +
+                "ON: " + date + "/n" +
+                "TEXT: " + text;
     }
 }
