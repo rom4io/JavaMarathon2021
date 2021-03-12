c85bf41f08626585392b93f72dc60158b36b9537
Rom4io <salyahov@gmail.com>
2021-03-12 09:32:52 +0300

    Add Day16 solution

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,37 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("Test3");
+        System.out.printf(printResult(file) + "-->" + "%.3f", printResult(file));
+    }
 
+    public static double printResult(File file) {
+        double ave = 0;
+        double sum = 0;
+        double counter = 0;
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] numbersStr = line.split(" ");
+            for (String number : numbersStr) {
+                sum += Integer.parseInt(number);
+                counter++;
+            }
+            ave = sum / counter;
+        }
+        return ave;
     }
 }
+
+
 
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,61 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Task2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File file1 = new File("file1");
+        File file2 = new File("file2");
+        PrintWriter pw1 = new PrintWriter(file1);
+        PrintWriter pw2 = new PrintWriter(file2);
+
+
+        for (int i = 0; i < 1000; i++) {
+            double rand = (int) (Math.random() * 100);
+            pw1.println(rand);
+        }
+        pw1.close();
+
+
+    /*    BufferedReader reader = new BufferedReader(new FileReader("file1.txt"));
+        while (reader.readLine() != null) {
+            for (int i = 0; i < 50; i++) {
+                int sum = 0;
+                for (int q = 0; q < 20; q++) {
+                    sum += Integer.parseInt(reader.readLine());
+                }
+                pw2.println(sum/20);
+            }*/
+
+
+        Scanner scanner = new Scanner(file1);
+        while (scanner.hasNext()) {
+            for (int i = 0; i < 50; i++) {
+                double sum = 0;
+                for (int q = 0; q < 20; q++) {
+                    sum += Double.parseDouble(scanner.nextLine());
+                }
+                pw2.println(sum/20);
+            }
+            pw2.close();
+        }
+        scanner.close();
+        printResult(file2);
+    }
+
+    public static void printResult(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        double sum = 0;
+        while (scanner.hasNextLine()){
+            sum += Double.parseDouble(scanner.nextLine());
+        }
+        System.out.println((int)sum);
 
     }
 }
+
+
